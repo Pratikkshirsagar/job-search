9e4c1267ca92cc51dd614d19fee1db39bc7f57e5
Pratikkshirsagar <meetpratiik1995@gmail.com>
2023-02-13 20:46:49 +0530

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    {
+      "prettier/prettier": [
+        "error",
+        {
+          "singleQuote": true,
+          "parser": "flow"
+        }
+      ]
+    }
+  },
+};
