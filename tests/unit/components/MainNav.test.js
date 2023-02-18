24a48eae7568860d574df6f90cde06ee02db41e5
Pratikkshirsagar <meetpratiik1995@gmail.com>
2023-02-18 19:44:24 +0530

    main nav ui and test done

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,20 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Bobo Careers');
     expect(companyName).toBeInTheDocument();
   });
+
+  it('displays menu items form navigation', () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole('listitem');
+    const navigationMenuText = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuText).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Bobo Corp',
+      'How we Hire',
+      'Students',
+      'Jobs',
+    ]);
+  });
 });
