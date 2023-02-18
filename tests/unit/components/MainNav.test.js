f98e517527134ea976f52aa78b6a10f38a8149d9
Pratikkshirsagar <meetpratiik1995@gmail.com>
2023-02-18 21:28:48 +0530

    mainnav signin button and img added

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
 import MainNav from '@/components/MainNav.vue';
 
 describe('MainNav', () => {
@@ -22,5 +23,25 @@ describe('MainNav', () => {
       'Students',
       'Jobs',
     ]);
+  });
+
+  describe('when the user login', () => {
+    it('displayes the user profile picture', async () => {
+      render(MainNav);
+      let profileImage = screen.queryByRole('img', {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole('button', {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole('img', {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
